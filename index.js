7ac3c5e7c076bcd6cbf0f60d4aacb75a8bf9a79a
“seetha1730” <seetharaj1990@gmail.com>
2023-08-17 21:53:28 +0200

    iteration 1 done

--- index.js
+++ index.js
@@ -17,6 +17,18 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    const receipe=  new Schema({
+      title: { type: String, required: true,unique:true },
+      level:{type:String,enum:["Easy Peasy" ,"Amateur Chef" ,"UltraPro Chef"]} ,
+      ingredients:{type:[ String ]},
+      cuisine:{type:String,required: true},
+      dishType:{type:String ,enum:["breakfast", "main_course", "soup", "snack", "drink", "dessert","other"]},
+      image:{type:String,default:"https://images.media-allrecipes.com/images/75131.jpg"},
+      duration:{type:Number,min: 0},
+      creator:{type:String},
+      created:{type:Date,default:Date.now}
+    })
+    
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
